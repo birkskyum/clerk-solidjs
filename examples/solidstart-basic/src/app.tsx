b82d6448ad87ad85ff965221a8da3a929677fab5
Birk Skyum <birk.skyum@pm.me>
2025-11-20 18:18:36 +0100

    update solidjs packages

--- examples/solidstart-basic/src/app.tsx
+++ examples/solidstart-basic/src/app.tsx
@@ -1,6 +1,5 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { ClerkProvider } from "clerk-solidjs-solidstart/start";
 import { Suspense } from "solid-js/web";
 
 import "./app.css";
@@ -8,13 +7,7 @@ import "./app.css";
 export default function App() {
   return (
     <Router
-      root={(props) => (
-        <ClerkProvider
-          publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
-        >
-          <Suspense>{props.children}</Suspense>
-        </ClerkProvider>
-      )}
+      root={(props) => <Suspense>{props.children}</Suspense>}
     >
       <FileRoutes />
     </Router>
